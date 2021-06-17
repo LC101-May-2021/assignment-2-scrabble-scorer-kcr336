21a8a9b6a1f8139e02f9c88bc00f5270a5ab3e34
Karen Beisel <46976285+kcr336@users.noreply.github.com>
2021-06-17 13:56:10 +0000

    newPointStructure added

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -19,17 +19,14 @@ const vowelPointStructure = {
 
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
-	let letterPoints = "";
+	let letterPoints = 0;
 	for (let i = 0; i < word.length; i++) {
- 
-	  for (const pointValue in oldPointStructure) {
- 
+	  for (const pointValue in oldPointStructure){
 		 if (oldPointStructure[pointValue].includes(word[i])) {
-			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+			letterPoints = letterPoints += (Number(pointValue))
 		 }
- 
 	  }
-	}
+	}console.log(`Points for ${word}: ${letterPoints}`);
 	return letterPoints;
  }
 
@@ -94,12 +91,38 @@ function scorerPrompt(word) {
 
 function transform() {}
 
-let newPointStructure="";
+const newPointStructure= {
+  'A': 1,
+  'B': 3,
+  'C': 3,
+  'D': 2,
+  'E': 1,
+  'F': 4,
+  'G': 2,
+  'H': 4,
+  'I': 1,
+  'J': 8,
+  'K': 5,
+  'L': 1,
+  'M': 3,
+  'N': 1,
+  'O': 1,
+  'P': 3,
+  'Q': 10,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'U': 1,
+  'V': 4,
+  'W': 4,
+  'X': 8,
+  'Y': 4,
+  'Z': 10
+}
 
 function runProgram() {
    let promptWord = initialPrompt();
    let promptChoice = scorerPrompt(promptWord);
-   console.log
 }
 
 
